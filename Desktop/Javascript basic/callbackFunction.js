4a8400742510e536f38f38399d2bac6037d1d52d
Danish Qayyum <danishbinqayyum123@gmail.com>
2025-06-27 01:48:26 -0700

    added a new files

---
+++ Desktop/Javascript basic/callbackFunction.js
@@ -0,0 +1,11 @@
+
+// -------------------( callback functions )---------//
+/*function sum(a,b) {
+    return a+b;
+    
+}
+function calc(fx,a,b) {
+     return fx(a,b)
+    
+}
+ console.log(clac(sum ,4 ,5));*/
